20ed73d6469e0f26d370d0399d119f0cc9f71f45
Jake Neuwelt <ekajn@hotmail.com>
2024-01-13 13:51:39 -0800

    Added HTML parser, title and UPC

--- main.py
+++ main.py
@@ -1,48 +1,86 @@
 import requests
+import re
+import csv
 from bs4 import BeautifulSoup
 
 # get url to scrape and use get to retrieve html code from product on site
 url = "http://books.toscrape.com/catalogue/sorting-the-beef-from-the-bull-the-science-of-food-fraud-forensics_736/index.html"
 page = requests.get(url)
 
-
-print(page.content)
-#
+# turn html into readable code
 soup = BeautifulSoup(page.content, "html.parser")
+#print(soup.prettify())
 
-for link in soup.find_all('a'):
-    print(link.get('href'))
-#url = soup.find_all('a', href_="../category/books_1/index.html")
-#print (url)
+content = soup.find('ul', class_='breadcrumb')
 
 
+url = "http://books.toscrape.com/catalogue/sorting-the-beef-from-the-bull-the-science-of-food-fraud-forensics_736/index.html"
+print(url)
+
+#Method 1 of getting UPC
+UPC= soup.find('td').get_text()
+print("The upc code of the book is: " + UPC)
+#Method 2 of getting UPC
 upc = soup.table.td
-print(upc.string)
+print("The upc code of the book is: " + upc.string)
 
 title = soup.title.string
-print(title)
+print("The title of the book is: " + title)
 
-priceWithTax = soup.price
-print(priceWithTax)
+priceWithTax = '<th>Price (incl. tax)</th><td>£44.74</td>'
+soup = BeautifulSoup(priceWithTax, 'html.parser')
+print("The price of the book with tax is: " + soup.td.get_text())
 
-priceNoTax = soup.price
-print(priceNoTax)
+priceNoTax = '<th>Price (excl. tax)</th><td>£44.74</td>'
+soup = BeautifulSoup(priceNoTax, 'html.parser')
+print("The price of the book without tax is: " + soup.td.get_text())
 
-quantity = soup.Availability
-print(quantity)
+#Method 1 to get quantity available
+"""
+soup = BeautifulSoup(page.content, "html.parser")
+quant = soup.find(string=re.compile("stock"))
+print("The availability of the book is: " + quant)
+"""
+#Method 2 to get quantity available
+quantity = '<td>In stock (14 available)</td>'
+soup = BeautifulSoup(quantity, 'html.parser')
+print("The availability of the book is: " + soup.td.get_text())
 
-sample = ''
-description = soup.find_all(class_="sub-header")
-print(description)
+soup = BeautifulSoup(page.content, "html.parser")
+content = soup.find(id='product_description').find_next_sibling()
+#print(content)
+description = content.get_text(strip=True)
 
-sample = '<a href="../category/books/science_22/index.html">Science</a>"'
-soup = BeautifulSoup(sample, "html.parser")
-print(soup.get_text())
-#print(category)
+print("The description of the book is: " + description)
 
-review = soup.review
-print(review)
+# = content.css.select("div > ul > li > a:nth-of-type(2)")
+#print(content)
+content = soup.find('ul', class_="breadcrumb")
+category = content.find_all("a")
+print("The category of the book is: ", category[len(category)-1].get_text())
+"""
+for x in category:
+    print(x)
+    """
 
-sample = ""
-soup = BeautifulSoup(sample, "html.parser")
-image = soup.image
+soup = BeautifulSoup(page.content, "html.parser")
+
+content = soup.find('p', class_="star-rating Four")
+print(content)
+review = content.find()
+
+content = soup.find("div", class_="item active")
+#print(content)
+image = content.find("img")
+#print(image)
+#print("The current review rating of the book is: " + image.get_text())
+
+#Create header list
+headers = ["product_page_url", "universal_ product_code (upc)", "book_title",
+           "price_including_tax", "price_excluding_tax", "quantity_available",
+           "product_description", "category", "review_rating", "image_url"]
+
+#Create new CSV to write to
+with open('Book.csv', 'w', newline = '') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    writer.writerow(headers)
