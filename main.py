6ce74716628705e405c67a0d5ecf6d29dc996aa2
Jake Neuwelt <ekajn@hotmail.com>
2024-01-19 00:14:16 -0800

    Revamped the Code again to be based off categories and changed the CSV section to print out CSV files per category

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import os
+
 import requests
 from bs4 import BeautifulSoup
+import re
 import csv
 
 # Get main page URL that will be used to create html to work off of
@@ -9,9 +12,9 @@ info = requests.get(page_url)
 # turn html into soup object that we will sort through
 soup = BeautifulSoup(info.content, "html.parser")
 #print(soup.prettify())
-books = soup.find_all('h3')
+#books = soup.find_all('h3')
 #print(books)
-
+"""
 #this gets information from one book
 book_url = "http://books.toscrape.com/catalogue/sorting-the-beef-from-the-bull-the-science-of-food-fraud-forensics_736/index.html"
 book_info = requests.get(book_url)
@@ -35,16 +38,23 @@ description = book_soup.find('div', id="product_description").find_next_sibling(
 #print(description)
 image = book_soup.find('div', class_="item active").find("img").get('src').strip()
 #print(image)
-#print(' ')
+
+"""
+"""
+category_url = "http://books.toscrape.com/catalogue/category/books/travel_2/index.html"
+category_info = requests.get(category_url)
+soup = BeautifulSoup(category_info.content, 'html.parser')
 
 #This creates a for loop that gets info from each book on one page
+books = soup.find_all('h3')
+#print(books)
+books_data = []
 for book in books:
-    book_url = book.find('a')['href']
+    book_url = book.find('a')['href'][9:] #problem is .../.../ so cut off leading 9 elements
     #print(book_url)
-    book_info = requests.get(page_url + book_url) #maybe add +page_url
+    book_info = requests.get('http://books.toscrape.com/catalogue/' + book_url) #instead of page_url + book_url
     #print(book_info)
     book_soup = BeautifulSoup(book_info.content, 'html.parser')
-
     upc = book_soup.find('table', class_="table table-striped").find_all('td')[0].text.strip()
     #print(upc)
     title = book_soup.find('h1').text
@@ -64,150 +74,76 @@ for book in books:
     image = book_soup.find('div', class_="item active").find("img").get('src').strip()
     #print(image)
     #print(' ')
+    books_data.append([book_url, upc, title, priceWithTax, priceNoTax, availability, description, category, rating,
+                       image])
+
+"""
 
 #Creates list for data on all books on all pages
 books_data = []
+"""
 for page_num in range(1, 51):
     url = f'http://books.toscrape.com/catalogue/page-{page_num}.html'
-    print(url)
+    #print(url)
     info = requests.get(url)
+    #print(info)
     soup = BeautifulSoup(info.content, 'html.parser')
+    print(soup)
 
     books = soup.find_all('h3')
     print(books)
+"""
 
+#categories = soup.find('ul', class_="nav nav-list").find('li').find('ul').find_all('a')
+categories = soup.find('div', class_="side_categories").find_all('li')#soup.find('ul', class_="nav nav-list").find_all('li')
+#print(categories)
+
+for cat in categories[1:]:
+    category_url = cat.find('a')['href']
+    #print(category_url)
+    category_info = requests.get(page_url + category_url)
+    #print(category_info)
+    category_soup = BeautifulSoup(category_info.content, 'html.parser')
+    #print(category_soup)
+    books = category_soup.find_all('h3')
+    #print(books)
     # This creates a for loop that gets info from each book on page
     for book in books:
-        book_url = book.find('a')['href']
-        book_info = requests.get(page_url + book_url)
+        book_url = book.find('a')['href'][9:]
+        book_info = requests.get('http://books.toscrape.com/catalogue/' + book_url) #this needs fixing
         book_soup = BeautifulSoup(book_info.content, 'html.parser')
 
         upc = book_soup.find('table', class_="table table-striped").find_all('td')[0].text.strip()
         title = book_soup.find('h1').text
-        priceWithTax = book_soup.find('table', class_="table table-striped").find_all('td')[2].text.strip()
-        priceNoTax = book_soup.find('table', class_="table table-striped").find_all('td')[3].text.strip()
+        priceWithTax = book_soup.find('table', class_="table table-striped").find_all('td')[2].text.strip()#.replace('£', '')
+        priceNoTax = book_soup.find('table', class_="table table-striped").find_all('td')[3].text.strip()#.replace('£', '')
         category = book_soup.find('ul', class_="breadcrumb").find_all('a')[2].text.strip()
         rating = book_soup.find('p', class_="star-rating")['class'][1]
         availability = book_soup.find('p', class_="instock availability").text.strip()
-        description = book_soup.find('div', id="product_description").find_next_sibling().text.strip()
+        description = book_soup.find_all('p')[3].text.strip()
+        #description = book_soup.find('div', id="product_description").find_next_sibling().text.strip()
         image = book_soup.find('div', class_="item active").find("img").get('src').strip()
 
-        books_data.append([upc, title, category, priceWithTax, priceNoTax, availability, description, category, rating,
+        books_data.append([book_url, upc, title, priceWithTax, priceNoTax, availability, description, category, rating,
                            image])
-        print(books_data)
+        #print(books_data)
+        # Create header list
+        headers = ["product_page_url", "universal_ product_code (upc)", "book_title",
+                   "price_including_tax", "price_excluding_tax", "quantity_available",
+                   "product_description", "category", "review_rating", "image_url"]
+
+        # Create new CSV to write to and add headers row
+        with open(f'{category}.csv', 'w', encoding='utf-8', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            writer.writerow(headers)
+
+            for i in range(len(books_data)):
+                row = books_data[i]
+                writer.writerow(row)
+    books_data.clear()
 
 
-"""
-content = soup.find('article', class_='product_pod')
-
-#Retrieve the url of book
-book_url = "http://books.toscrape.com/catalogue/sorting-the-beef-from-the-bull-the-science-of-food-fraud-forensics_736/index.html"
-print(book_url)
-
-#Method 1 of getting UPC
-book_upc= soup.find('td').get_text()
-print("The upc code of the book is: " + book_upc)
-#Method 2 of getting UPC
-upc = soup.table.td
-print("The upc code of the book is: " + upc.string)
-
-#Get the title of the book
-book_titles = soup.title
-print("The title of the book is: " + book_titles.get_text(strip=True))
-
-#Get the price with Tax
-priceWithTax = '<th>Price (incl. tax)</th><td>£44.74</td>'
-soup = BeautifulSoup(priceWithTax, 'html.parser')
-print("The price of the book with tax is: " + soup.td.get_text())
-
-#Get the price without Tax
-priceNoTax = '<th>Price (excl. tax)</th><td>£44.74</td>'
-soup = BeautifulSoup(priceNoTax, 'html.parser')
-print("The price of the book without tax is: " + soup.td.get_text())
-
-#Method 1 to get quantity available
-
-soup = BeautifulSoup(page.content, "html.parser")
-quant = soup.find(string=re.compile("stock"))
-print("The availability of the book is: " + quant)
 
 
-#Method 2 to get quantity available
-quantity = '<td>In stock (14 available)</td>'
-soup = BeautifulSoup(quantity, 'html.parser')
-print("The availability of the book is: " + soup.td.get_text())
 
-#Get the description of the Book
-soup = BeautifulSoup(page.content, "html.parser")
-content = soup.find(id='product_description').find_next_sibling()
-description = content.get_text(strip=True)
-print("The description of the book is: " + description)
 
-#Geth the category of the Book
-content = soup.find('ul', class_="breadcrumb")
-category = content.find_all("a")
-print("The category of the book is: ", category[len(category)-1].get_text())
-
-#redefine the soup to allow for searching
-soup = BeautifulSoup(page.content, "html.parser")
-
-#To get the review of the Book
-content = soup.find('p', class_="star-rating Four")
-print("The rating of the book is: " + content.name)
-#review = content
-#print(review)
-
-#To get the review URL
-content = soup.find("div", class_="item active")
-image = content.find("img")
-print("The image url is: " +image.get('src'))
-
-#lists to hold needed categorys
-TITLES = []
-for title in book_titles:
-    TITLES.append(title)
-URLS = []
-for url in book_url:
-    URLS.append(url)
-    print(URLS)
-UPCS = []
-for upc in book_upc:
-    UPCS.append(upc)
-PRICES_WTAX = []
-for price in priceWithTax:
-    PRICES_WTAX.append(price)
-PRICES_NTAX = []
-for price in priceNoTax:
-    PRICES_NTAX.append(price)
-QUANTITY = []
-for num in quantity:
-    QUANTITY.append(num)
-DESCRIPTIONS = []
-for info in description:
-    DESCRIPTIONS.append(description)
-CATEGORIES = []
-for cat in category:
-    CATEGORIES.append(cat)
-RATINGS = []
-for title in book_titles:
-    RATINGS.append(title)
-IMAGE_URLS =[]
-for link in image:
-    IMAGE_URLS.append(link)
-
-#Create header list
-headers = ["product_page_url", "universal_ product_code (upc)", "book_title",
-           "price_including_tax", "price_excluding_tax", "quantity_available",
-           "product_description", "category", "review_rating", "image_url"]
-
-#Create new CSV to write to and add headers row
-with open('Book.csv', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile, delimiter=',')
-    writer.writerow(headers)
-
-    for i in range(len(TITLES)):
-        print(TITLES)
-        row = [URLS[i], UPCS[i], TITLES[i], PRICES_WTAX[i], PRICES_NTAX[i], QUANTITY[i], DESCRIPTIONS[i], CATEGORIES[i], RATINGS[i], IMAGE_URLS[i]]
-        writer.writerow(row)
-
-"""
