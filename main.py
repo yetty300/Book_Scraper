0b8f7d495bd29307ec628327f3996df858bb14dd
yetty300 <114951417+yetty300@users.noreply.github.com>
2024-01-20 22:39:11 -0800

    Added functionality to retrieve and store book images. Changed the soup code in previous sections so they didn't interfere with future sections. Changed the urls code to more accurately reflect the total url for those sections.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from urllib.parse import urljoin
+import re
 import requests
 from bs4 import BeautifulSoup
 import csv
+import os
 
 # Get main page URL that will be used to create html to work off of
 page_url = "http://books.toscrape.com/"
@@ -26,14 +29,14 @@ image = book_soup.find('div', class_="item active").find("img").get('src').strip
 
 
 
-category_url = "http://books.toscrape.com/catalogue/category/books/travel_2/index.html"
-category_info = requests.get(category_url)
-soup = BeautifulSoup(category_info.content, 'html.parser')
+category_url_pre = "http://books.toscrape.com/catalogue/category/books/travel_2/index.html"
+category_info_pre = requests.get(category_url_pre)
+next_soup = BeautifulSoup(category_info_pre.content, 'html.parser')
 
 #This creates a for loop that gets info from each book on one page
-books = soup.find_all('h3')
+books_pre = next_soup.find_all('h3')
 books_data_pre = []
-for book in books:
+for book in books_pre:
     book_url = book.find('a')['href'][9:]
     book_info = requests.get('http://books.toscrape.com/catalogue/' + book_url)
     book_soup = BeautifulSoup(book_info.content, 'html.parser')
@@ -49,11 +52,27 @@ for book in books:
     books_data_pre.append([book_url, upc, title, priceWithTax, priceNoTax, availability, description, category, rating,
                        image])
 
+    # Create header list
+    headers = ["product_page_url", "universal_ product_code (upc)", "book_title",
+               "price_including_tax", "price_excluding_tax", "quantity_available",
+               "product_description", "category", "review_rating", "image_url"]
+
+    # Create new CSV to write to and add headers row
+    with open('SingleCategory.csv', 'w', encoding='utf-8', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',')
+        writer.writerow(headers)
+
+        for i in range(len(books_data_pre)):
+            row = books_data_pre[i]
+            writer.writerow(row)
+
+
+
 
 
 #Creates list for data on all books on all pages
 books_data = []
-
+os.makedirs('book images', exist_ok=True)
 categories = soup.find('div', class_="side_categories").find_all('li')
 
 for cat in categories[1:]:
@@ -67,20 +86,27 @@ for cat in categories[1:]:
         book_url = book.find('a')['href'][9:]
         book_info = requests.get('http://books.toscrape.com/catalogue/' + book_url)
         book_soup = BeautifulSoup(book_info.content, 'html.parser')
+        base_url = 'http://books.toscrape.com/catalogue/'
+        book_url_final = urljoin(base_url, book_url)
 
         upc = book_soup.find('table', class_="table table-striped").find_all('td')[0].text.strip()
         title = book_soup.find('h1').text
+        valid_title = re.sub(r'[\/:*?"<>|]', '', title)
         priceWithTax = book_soup.find('table', class_="table table-striped").find_all('td')[2].text.strip()
         priceNoTax = book_soup.find('table', class_="table table-striped").find_all('td')[3].text.strip()
         category = book_soup.find('ul', class_="breadcrumb").find_all('a')[2].text.strip()
         rating = book_soup.find('p', class_="star-rating")['class'][1]
         availability = book_soup.find('p', class_="instock availability").text.strip()
         description = book_soup.find_all('p')[3].text.strip()
-        image = book_soup.find('div', class_="item active").find("img").get('src').strip()
+        image = book_soup.find("img")
+        image_url = urljoin(book_url_final, image['src'])
+        img_response = requests.get(image_url)
 
-        books_data.append([book_url, upc, title, priceWithTax, priceNoTax, availability, description, category, rating,
-                           image])
+        books_data.append([book_url_final, upc, title, priceWithTax, priceNoTax, availability, description, category,
+                           rating, image_url])
 
+        with open(f'book images/{valid_title}.jpg', 'wb') as img_file:
+            img_file.write(img_response.content)
         # Create header list
         headers = ["product_page_url", "universal_ product_code (upc)", "book_title",
                    "price_including_tax", "price_excluding_tax", "quantity_available",
@@ -95,6 +121,9 @@ for cat in categories[1:]:
                 row = books_data[i]
                 writer.writerow(row)
     books_data.clear()
+
+
+
 
 
 
